dc2bbea02b0077d98e01ce5122d8a8547093c350
kongdepu <kongdepu@126.com>
2023-05-18 20:44:03 +0800

    'master'

--- src/main/java/com/kdp/web/GitTest.java
+++ src/main/java/com/kdp/web/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello 111");
         System.out.println("hello hot");
+        System.out.println("hello master");
     }
 }
