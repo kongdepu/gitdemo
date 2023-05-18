0199d21858dbda687a88d374d0e59d8408de40df
kongdepu <kongdepu@126.com>
2023-05-18 20:42:59 +0800

    'hot'

--- src/main/java/com/kdp/web/GitTest.java
+++ src/main/java/com/kdp/web/GitTest.java
@@ -3,5 +3,6 @@ package com.kdp.web;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello 111");
+        System.out.println("hello hot");
     }
 }
