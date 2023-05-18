d59791b9166ecfe83fa9d4b34b95d2cd555ac896
kongdepu <kongdepu@126.com>
2023-05-18 21:03:36 +0800

    'mer'

--- src/main/java/com/kdp/web/GitTest.java
+++ src/main/java/com/kdp/web/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello master");
         System.out.println("hello hot 11111");
         System.out.println("hello hot 22222");
+        System.out.println("开始把");
     }
 }
