08cb2f2b4be12badfb10f295a9734ab2f31a8829
kongdepu <87957798+kongdepu@users.noreply.github.com>
2023-05-18 20:57:58 +0800

    Update GitTest.java

--- src/main/java/com/kdp/web/GitTest.java
+++ src/main/java/com/kdp/web/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello hot");
         System.out.println("hello master");
         System.out.println("hello hot 11111");
+        System.out.println("hello hot 22222");
     }
 }
