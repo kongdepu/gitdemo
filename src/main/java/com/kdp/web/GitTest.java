449c861500bacfdf2e7fe517592c6abd0824af86
kongdepu <kongdepu@126.com>
2023-05-19 10:24:51 +0800

    '第二个版本了哦'

--- src/main/java/com/kdp/web/GitTest.java
+++ src/main/java/com/kdp/web/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello hot 11111");
         System.out.println("hello hot 22222");
         System.out.println("开始把");
+        System.out.println("我来写第二个版本吧!");
     }
 }
